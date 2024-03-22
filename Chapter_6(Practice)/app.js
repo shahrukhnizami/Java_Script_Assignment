b75e3fd09da0e6e3891577e53130d0ab1008ac57
shahrukhnizami <154953847+shahrukhnizami@users.noreply.github.com>
2024-03-22 07:16:11 +0500

    sds

---
+++ Chapter_6(Practice)/app.js
@@ -0,0 +1,74 @@
+document.write( `<h1>1-Write a loop to print numbers from 1 to 10.</h1><br>`);
+
+for(var i = 1; i <= 10; i++){
+    document.write(i + "<br>");
+}
+
+document.write( `<h1>2-Write a loop to print even numbers from 1 to 20.</h1><br>`);
+for(var i = 1; i <= 20; i++){
+    if(i % 2 == 0){
+        document.write(i + "<br>");
+    }
+}
+
+document.write( `<h1>3-Write a loop to print odd numbers from 1 to 15.</h1><br>`);
+for(var i = 1; i <= 15; i++){
+    if(i % 2 !== 0){
+        document.write(i + "<br>");
+    }
+}
+
+
+document.write( `<h1>4-Write a loop to calculate and print the factorial of a number (let's say 5).</h1><br>`);
+var num = prompt("Enter a number to calculate its factorial: ");
+var factorial = 1;
+for (let i = 1; i <= num; i++) {
+    factorial *= i;
+}
+document.write(`The factorial of ${num} is: ${factorial}`);
+
+document.write( `<h1>5-Write a loop to print the multiplication table of a number (let's say 7) up to 10.</h1><br>`);
+
+var userInput = prompt("Enter a number to print its multiplication table: ");
+for(var i = 1; i <= 10; i++){
+    document.write(`${userInput} x ${i} = ${userInput * i}<br>`);
+}
+document.write(`<h1>6-Squares of numbers from 1 to 10:</h1>`);
+for (let i = 1; i <= 10; i++) {
+    document.write(`${i} squared is: ${i * i}<br>`);
+}
+
+document.write(`<h1>7- Write a loop to find and print the sum of digits of a number (let's say 123).
+<br>Write a loop to print the reverse of a string (let's say "hello").</h1>`);
+
+
+
+var userInput = +prompt("Enter a number to print a string 10 times: ");
+for (let i = 1; i <= userInput; i++) {
+    document.write(`${i} Hello World!<br>`);
+}
+
+document.write(`<h1>8- Write a loop to find and print the smallest element in an array (let's say [3, 7, 2, 9, 5]).</h1>`);
+var arr = [3, 7, 2, 9, 5];
+var smallest = arr[0];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+        smallest = arr[i];
+    }
+}
+document.write(`The array is: ${arr}<br>`);
+document.write(`The smallest number in the array is: ${smallest}`);
+
+
+document.write(`<h1>9-  Write a loop to find and print the largest element in an array (let's say [3, 7, 2, 9, 5]).</h1>`);
+var arr = [3, 7, 2, 9, 5];
+var largest = arr[0];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+}
+document.write(`The array is: ${arr}<br>`);
+document.write(`The largest number in the array is: ${largest}`);
+
+
